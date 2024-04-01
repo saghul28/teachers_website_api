70eaed097205e46ef832c58f569bd8322749a664
saghul28 <saghulhameed30@gmail.com>
2024-04-01 11:08:33 +0530

    first commit

---
+++ Routers/Account/UserAuth.py
@@ -0,0 +1,72 @@
+from fastapi import APIRouter, Depends, Header
+
+from pydantic import BaseModel, EmailStr
+from FireDatabase import pyreauth
+from firebase_admin import auth
+from fastapi import Header, HTTPException
+from fastapi.security import HTTPAuthorizationCredentials,HTTPBearer
+
+router = APIRouter()
+security = HTTPBearer()
+
+class Register(BaseModel):
+    username: str
+    password: str
+    email: EmailStr
+    phone_number: int
+
+
+@router.post("/signup")
+async def signup(register: Register):
+    display_name = register.username
+    email = register.email
+    phone_number = register.phone_number
+    password = register.password
+    try:
+        auth.create_user(email=email, password=password, display_name=display_name)
+        return {"message": email}
+    except Exception as e:
+        return {"message": str(e)}
+
+
+class Login(BaseModel):
+    email: EmailStr
+    password: str
+
+
+@router.post("/login")
+async def login(userlogin: Login):
+    email = userlogin.email
+    password = userlogin.password
+    try:
+        user = pyreauth.sign_in_with_email_and_password(email=email, password=password)
+
+        return {"token": user["idToken"],"localId": user["localId"]}
+    except Exception as e:
+        return {"message": str(e)}
+
+
+
+def validate_token(token: str):
+    try:
+        user = auth.verify_id_token(token)
+        return user
+    except Exception as e:
+        raise Exception(status_code=401, detail="Invalid token")
+
+
+def authenticate_token(credentials: HTTPAuthorizationCredentials = Depends(security)):
+    try:
+        if credentials is None:
+            raise HTTPException(status_code=401, detail="Authorization header missing")
+
+        token = credentials.credentials
+        coded_token = auth.verify_id_token(token)
+        user = auth.get_user(coded_token["uid"])
+        return user
+    except Exception as e:
+        raise HTTPException(status_code=401, detail="Invalid authorization header")
+
+@router.get("/protected")
+def protected(token: str = Depends(authenticate_token)):
+    return {"message": token}
---
+++ Routers/Blog/blog.py
@@ -0,0 +1,85 @@
+from fastapi import APIRouter, Depends, Header, HTTPException
+from pydantic import BaseModel
+from Routers.Account.UserAuth import authenticate_token
+from firebase_admin import firestore, auth
+
+router = APIRouter()
+
+store = firestore.client()
+
+
+class Post(BaseModel):
+    title: str
+    content: str
+
+
+# Create a blog post
+
+@router.post("/posts")
+async def create_post(post: Post, user: str = Depends(authenticate_token)):
+    try:
+        title = post.title
+        content = post.content
+        author = user.display_name
+        uid = user.uid
+
+        data = {"title": title, "content": content, "author": author}
+
+        transaction = store.transaction()
+
+        post_id = store.collection("Blogs").document(uid).collection("UserPosts").document().id
+
+
+        user_post_ref = store.collection("Blogs").document(uid).collection("UserPosts").document(post_id)
+        transaction.set(user_post_ref, data)
+
+
+        all_blogs_ref = store.collection("All Blogs").document(post_id)
+        transaction.set(all_blogs_ref, data)
+
+        transaction.commit()
+        return {"message": "Blog post created successfully"}
+    except Exception as e:
+        return {"message": f"Error: {e}"}
+
+
+# Get all blogs of the current user
+@router.get("/get_user_blogs")
+async def get_user_blogs(current_user: str = Depends(authenticate_token)):
+    try:
+        uid = current_user.uid
+        blogs_ref = store.collection("Blogs").document(uid).collection("UserPosts")
+        docs = blogs_ref.get()
+        blogs_data = []
+        for doc in docs:
+            blogs_data.append(doc.to_dict())
+        return {"blogs": blogs_data}
+    except Exception as e:
+        return {"message": f"Error: {e}"}
+    
+@router.get("/blogs")
+async def get_all_blogs():
+    try:
+        blogs_data = []
+        blogs_ref = store.collection("All Blogs")
+        query_snapshot = blogs_ref.get()
+        for doc in query_snapshot:
+            blogs_data.append(doc.to_dict())
+
+        return {"blogs": blogs_data}
+
+    except Exception as e:
+        raise HTTPException(status_code=500, detail=f"Error: {e}")
+
+
+@router.delete(f"/delete_blogs/{id}")
+async def delete_blogs(id:str,current_user: str = Depends(authenticate_token)):
+    try:
+
+        blogs_ref = store.collection("Blogs").document(current_user.uid).collection("UserPosts").document(id)
+        blogs_ref.delete()
+        return {"message": "deleted successfully"}
+
+    except Exception as e:
+        raise HTTPException(status_code=500, detail=f"Error: {e}")
+

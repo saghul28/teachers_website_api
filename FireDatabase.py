a708e32664fc59b34d5db8e781b315f2aeaf5066
Saghul ahmed <saghulhameed30@gmail.com>
2024-04-03 11:15:28 +0530

    database

--- FireDatabase.py
+++ FireDatabase.py
@@ -32,6 +32,7 @@ cred = credentials.Certificate({
     "client_x509_cert_url": os.getenv("CLIENT_X509_CERT_URL"),
     "universe_domain": os.getenv("UNIVERSE_DOMAIN")
 })
+print(config)
 firebase_admin.initialize_app(cred)
 
 firebase_config = {
